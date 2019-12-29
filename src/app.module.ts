21723413c4cb3338772cbcbf288c1e52950a97c4
thefirstspine <teddy@coretizone.com>
2019-12-29 19:40:39 +0100

    Added TypeORM to app modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,26 @@
 import { Module } from '@nestjs/common';
-import { MessagingService } from './messaging/messaging.service';
 import { ApiController } from './api/api.controller';
 import { MessagingGateway } from './messaging/messaging.gateway';
 import { AuthService } from './@shared/auth-shared/auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import env from './@shared/env-shared/env';
+import { MessagingModule } from './messaging/messaging.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MessagingModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      synchronize: true,
+      entities: env.dist ? [__dirname + '/**/**.entity.js'] : [__dirname + '/**/**.entity{.ts,.js}'],
+      host: env.config.MESSAGING_PG_HOST,
+      port: env.config.MESSAGING_PG_PORT,
+      username: env.config.MESSAGING_PG_USERNAME,
+      password: env.config.MESSAGING_PG_PASSWORD,
+      database: env.config.MESSAGING_PG_DATABASE,
+    }),
+  ],
   controllers: [ApiController],
-  providers: [AuthService, MessagingService, MessagingGateway],
+  providers: [AuthService, MessagingGateway],
 })
 export class AppModule {}
--- src/messaging/messaging.module.ts
+++ src/messaging/messaging.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { MessagingGateway } from './messaging.gateway';
 import { MessagingService } from './messaging.service';
 import { AuthService } from '../@shared/auth-shared/auth.service';
+import { Message } from '../entities/message.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Message])],
   providers: [MessagingGateway, AuthService, MessagingService],
+  exports: [MessagingService],
 })
 export class MessagingModule {}

6a01ed49051d25db1491a7eb901aa933c36170ad
thefirstspine <teddy@coretizone.com>
2020-03-24 15:37:09 +0100

    Added global error filter

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WsAdapter } from '@nestjs/platform-ws';
 import env from './@shared/env-shared/env';
+import { ErrorFilter } from './error.filter';
+import { LogService } from './@shared/log-shared/log.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new WsAdapter(app));
   app.enableCors();
+  app.useGlobalFilters(new ErrorFilter(new LogService('messaging')));
   await app.listen(env.dist ? env.config.PORT : 2205);
 }
 bootstrap();

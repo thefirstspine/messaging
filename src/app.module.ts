68e98eaa70cc4ed38dcb10960153f058ab173121
thefirstspine <teddy@coretizone.com>
2020-06-02 20:22:52 +0200

    Removed used of env shared assets prior to dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,36 @@
-import { Module } from '@nestjs/common';
+import { Module, DynamicModule } from '@nestjs/common';
 import { ApiController } from './api/api.controller';
 import { MessagingGateway } from './messaging/messaging.gateway';
 import { AuthService } from './@shared/auth-shared/auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import env from './@shared/env-shared/env';
 import { MessagingModule } from './messaging/messaging.module';
 import { IndexController } from './index/index.controller';
 import { LogService } from './@shared/log-shared/log.service';
 
-@Module({
-  imports: [
-    MessagingModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      synchronize: true,
-      entities: env.dist ? [__dirname + '/**/**.entity.js'] : [__dirname + '/**/**.entity{.ts,.js}'],
-      host: env.config.PG_HOST,
-      port: env.config.PG_PORT,
-      username: env.config.PG_USERNAME,
-      password: env.config.PG_PASSWORD,
-      database: env.config.PG_DATABASE,
-    }),
-  ],
-  controllers: [ApiController, IndexController],
-  providers: [
-    AuthService,
-    MessagingGateway,
-    {provide: LogService, useValue: new LogService('messaging')},
-  ],
-})
-export class AppModule {}
+@Module({})
+export class AppModule {
+  public static register(): DynamicModule {
+    return {
+      module: AppModule,
+      imports: [
+        MessagingModule,
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          synchronize: true,
+          entities: [__dirname + '/**/**.entity{.ts,.js}'],
+          host: process.env.PG_HOST,
+          port: parseInt(process.env.PG_PORT, 10),
+          username: process.env.PG_USERNAME,
+          password: process.env.PG_PASSWORD,
+          database: process.env.PG_DATABASE,
+        }),
+      ],
+      controllers: [ApiController, IndexController],
+      providers: [
+        AuthService,
+        MessagingGateway,
+        {provide: LogService, useValue: new LogService('messaging')},
+      ],
+    };
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WsAdapter } from '@nestjs/platform-ws';
-import env from './@shared/env-shared/env';
 import { ErrorFilter } from './error.filter';
 import { LogService } from './@shared/log-shared/log.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // Load dotenv config
+  require('dotenv').config();
+
+  // Start app
+  const app = await NestFactory.create(AppModule.register());
   app.useWebSocketAdapter(new WsAdapter(app));
   app.enableCors();
   app.useGlobalFilters(new ErrorFilter(new LogService('messaging')));
-  await app.listen(env.dist ? env.config.PORT : 2205);
+  await app.listen(process.env.PORT);
 }
 bootstrap();

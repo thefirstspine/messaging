257c83c998f633119ed07a3008cf0fe23872ca95
thefirstspine <teddy@thefirstspine.fr>
2023-09-20 20:29:36 +0530

    Fixed tests

--- src/messaging/messaging.service.spec.ts
+++ src/messaging/messaging.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MessagingService } from './messaging.service';
+import { AuthService } from '@thefirstspine/auth-nest';
+import { MessagingGateway } from './messaging.gateway';
+import { LogsService } from '@thefirstspine/logs-nest';
 
 describe('MessagingService', () => {
   let service: MessagingService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MessagingService],
+      providers: [
+        AuthService,
+        MessagingService,
+        MessagingGateway,
+        LogsService,
+      ],
     }).compile();
 
     service = module.get<MessagingService>(MessagingService);
